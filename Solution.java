f5b8241940cb650079e30b27d847b94acfab6fcd
Ajay Rathore <43730286+LUCKYAJAYRATHORE@users.noreply.github.com>
2024-01-30 23:22:27 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+	public static void findCombinationsAll(int x, int n, ArrayList<Integer> list, Set < List<Integer>> combinations) {
+
+		if (x > n) {
+			combinations.add(new ArrayList<>(list));
+			System.out.println(combinations);
+			return;
+		}
+
+		for (int i = 0; i < 2*n; i++) {
+			if (list.get(i) == -1 && (i + x + 1)<2 * n && list.get(i + x + 1) == -1) {
+				list.set(i, x);
+				list.set(i + x + 1, x);
+
+				findCombinationsAll(x + 1, n, list, combinations);
+
+				list.set(i, -1);
+				list.set(i + x + 1, -1);
+				// combinations.remove(combinations);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Set < List<Integer>> combinations = new HashSet<>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < 2 * 3; i++) {
+			list.add(-1);
+		}
+		findCombinationsAll(1,4,list, combinations);
+			for(List<Integer> c : combinations){
+			System.out.println(c);
+		}
+		return;
+	}
+}

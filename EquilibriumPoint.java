8fba063e404081dba1297b5a6f6206de1212b7f0
Ajay Rathore <43730286+LUCKYAJAYRATHORE@users.noreply.github.com>
2025-09-14 02:25:59 +0530

    Add files via upload

---
+++ EquilibriumPoint.java
@@ -0,0 +1,41 @@
+
+
+class EquilibriumPoint {
+
+
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+
+        int sum = 0; // initialize sum of whole array
+        int leftsum = 0; // initialize leftsum
+
+        /* Find sum of the whole array */
+        for (int i = 0; i < n; ++i)
+            sum += arr[i];
+
+        for (int i = 0; i < n; ++i) {
+            sum -= arr[i]; // sum is now right sum for index
+                           // i
+
+            if (leftsum == sum)
+                return i+1;
+
+            leftsum += arr[i];
+        }
+
+        /* If no equilibrium index found, then return 0 */
+        return -1;
+}
+
+ public static void main (String[] args) {
+        int[] arr1 = {2, 3, -8, 7, -1, 2, 3};
+        long[] arr = new long[arr1.length];
+        EquilibriumPoint equilibrium = new EquilibriumPoint();
+        System.out.println(equilibrium.equilibriumPoint(arr, 7));
+
+}
+
+
+}
